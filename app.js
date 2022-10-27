b1975da66b65a05cafee1c629ba6f17e6bc58a2f
User <user@Users-MacBook-Pro.local>
2022-10-27 14:16:54 -0700

    resume_page

--- app.js
+++ app.js
@@ -3,3 +3,9 @@ const text = document.querySelectorAll('.thePaths');
 for (let i = 0; i< text.length; i++) {
     console.log(`text number ${i} length is ${text[i].getTotalLength()}`);
 }
+
+const lastWord = document.querySelector('#twelve');
+const animation = document.querySelector('div.animation');
+lastWord.addEventListener('animationend',()=>{
+    animation.style = 'transition:all 0.5s ease; opacity:0 ; pointer-events:none';
+})
